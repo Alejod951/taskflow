57284fa469206dc497bf39feecf0703a0911dfe5
Alejod951 <Alejod951@gmail.com>
2025-02-22 11:09:54 -0500

    Conexion entre frontend y backend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,16 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors()); // Permite peticiones desde el frontend
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("TaskFlow Backend is running! 🚀");
 });
+
+app.get("/api/test", (req, res) => {
+    res.json({ message: "¡Backend conectado correctamente!" });
+  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
